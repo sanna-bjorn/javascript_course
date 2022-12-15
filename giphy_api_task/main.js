cab520d153a755d1102dbda8d45c6e50202473e7
sartzalainen <peppikoira@gmail.com>
2022-12-15 14:18:38 +0200

    refactored to a simple version

--- giphy_api_task/main.js
+++ giphy_api_task/main.js
@@ -1,35 +1,18 @@
-const img = document.querySelector("img");
-function changeGifImage() {
-  fetch(
-    "https://api.giphy.com/v1/gifs/translate?api_key=zBsFqoIx3tj9BeXTAwLyKBWScuuvuzow&s=cat",
-    { mode: "cors" }
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      img.src = response.data.images.original.url;
-    })
-    .catch((e) => {
-      console.log(e);
-    });
-}
+// changeGifImage().then((image) => {
+//   document.body.innerHTML = getPizzaHtml(image);
+//   let changeBtn = document.getElementById("change_pic");
 
-changeGifImage().then((image) => {
-  document.body.innerHTML = getPizzaHtml(image);
-  let changeBtn = document.getElementById("change_pic");
-
-  changeBtn.addEventListener("click", (e) => {
-    let gifImg = document.querySelector(".gif_images");
-    if (gifImg.style.display === "none") {
-      gifImg.style.display = "inline";
-    } else {
-      changeGifImage().then((newGif) => (gifImg.src = newGif.image));
-    }
-  });
-});
-
-//   change_pic.addEventListener("click", (e) => {
-//     formModal.classList.remove("active");
-//     e.preventDefault();
+//   changeBtn.addEventListener("click", (e) => {
+//     let gifImg = document.querySelector(".gif_images");
+//     if (gifImg.style.display === "none") {
+//       gifImg.style.display = "inline";
+//     } else {
+//       changeGifImage().then((newGif) => (gifImg.src = newGif.image));
+//     }
 //   });
+// });
+
+// //   change_pic.addEventListener("click", (e) => {
+// //     formModal.classList.remove("active");
+// //     e.preventDefault();
+// //   });
