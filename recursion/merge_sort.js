063e7897dcb0b893f9decef3973716cf2c2e78cf
sartzalainen <peppikoira@gmail.com>
2023-01-03 10:48:26 +0200

    merge sort function

---
+++ recursion/merge_sort.js
@@ -0,0 +1,23 @@
+const mergeSort = (array) => {
+  if (array.length <= 1) return array;
+
+  const middlePoint = Math.floor(array.length / 2);
+  const leftHalf = mergeSort(array.slice(0, middlePoint));
+  const rightHalf = mergeSort(array.slice(middlePoint));
+  return merge(leftHalf, rightHalf);
+};
+
+const merge = (leftArray, rightArray) => {
+  const mergedCollection = [];
+
+  while (leftArray.length > 0 && rightArray.length > 0) {
+    const arrayWithMin = leftArray[0] < rightArray[0] ? leftArray : rightArray;
+    const mergeElement = arrayWithMin.shift();
+    mergedCollection.push(mergeElement);
+  }
+
+  return mergedCollection.concat(leftArray, rightArray);
+};
+
+console.log(mergeSort([5, 0, 10, -3, -1, 4, 12, -5])); // [-5, -3, -1, 0, 4, 5, 10, 12];
+console.log(mergeSort([])); // []
