6cc46a8e67c95432b9dde4054d3cc6c13b79025e
sartzalainen <peppikoira@gmail.com>
2023-01-03 10:40:24 +0200

    adding recursive option

--- recursion/fibonacci.js
+++ recursion/fibonacci.js
@@ -9,3 +9,15 @@ function fibs(fn) {
 }
 
 console.log(fibs(-2));
+
+function fibsRec(fn, array = [0, 1]) {
+  if (fn <= 0) return 'Please enter a positive integer.';
+  else if (fn === 1) return [0];
+  else if (array.length >= fn) return array;
+  return fibsRec(fn, [...array, array.at(-1) + array.at(-2)]);
+}
+
+console.log(fibsRec(-2)); // Please enter a positive integer.
+console.log(fibsRec(1)); // returns [0]
+console.log(fibsRec(2)); // returns [0, 1]
+console.log(fibsRec(8)); // returns [0, 1, 1, 2, 3, 5, 8, 13]
